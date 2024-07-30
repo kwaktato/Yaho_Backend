6fa8d07735788c88f053086fefc2f83c51448337
gayeon7877 <gayean01@gmail.com>
2024-07-30 17:46:57 +0900

    ✨ [Feature] 테이블 생성

---
+++ src/main/java/com/example/yaho/domain/Member.java
@@ -0,0 +1,39 @@
+package com.example.yaho.domain;
+
+import com.example.yaho.domain.common.BaseEntity;
+import com.example.yaho.domain.enums.FairyGrade;
+import jakarta.persistence.*;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(length = 15)
+    private String nickname;
+
+    @Column(name = "profile_image", length = 255)
+    private String profileImage;
+
+    @Column(name = "Field", length = 255)
+    private String field;
+
+    @Enumerated(EnumType.STRING)
+    private FairyGrade fairyGrade;
+
+    @Column(name = "created_at", columnDefinition = "DATETIME(6)")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", columnDefinition = "DATETIME(6)")
+    private LocalDateTime updatedAt;
+
+}
