3d438201ef93050c23b44e2662dd957a2f0fe1da
kwaktato <109459319+kwaktato@users.noreply.github.com>
2024-08-10 03:00:18 +0900

    [Feature] SecurityUtil 추가

---
+++ src/main/java/com/example/yaho/utils/SecurityUtil.java
@@ -0,0 +1,28 @@
+package com.example.yaho.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtil {
+
+    /* 현재 사용중인 유저의 id 가져오기
+    public static Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) { return null; }
+        Long userId = (Long) authentication.getPrincipal();
+
+        return userId;
+    }
+    */
+
+    // 현재 사용중인 유저의 nickname 가져오기
+    public static String getCurrentUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getName() == null) {
+            return null;
+        }
+        System.out.println("authentication = " + authentication.getName());
+        return authentication.getName();
+    }
+}
