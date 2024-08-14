b0f463789a48cff7b6e7b6f81fce4609ad29ff89
kwaktato <109459319+kwaktato@users.noreply.github.com>
2024-08-14 20:07:18 +0900

    [Comment] pathvariable을 사용함에 따른 security 관련 코드들 주석 처리

--- src/main/java/com/example/yaho/config/SecurityConfig.java
+++ src/main/java/com/example/yaho/config/SecurityConfig.java
@@ -1,35 +1,43 @@
-package com.example.yaho.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@RequiredArgsConstructor
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
-                        "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
-                        "/webjars/**", "/swagger-ui.html").permitAll()
-                        .anyRequest().authenticated()); // 일단 임시로 허용
-
-        return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        // 아래 url은 filter 에서 제외
-        return web ->
-                web.ignoring()
-                        .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**", "/auth/**");
-    }
-}
+//package com.example.yaho.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests((authorizeRequests) -> {
+//                    authorizeRequests.anyRequest().permitAll();
+//                });
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer(){
+//        return web ->
+//                web.ignoring().requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**");
+//    }
+//}
+//
+//
+//
+////        http
+////                .authorizeHttpRequests((auth) -> auth
+////                        .requestMatchers( "/swagger-resources",
+////                        "/swagger-resources/**", "/configuration/ui", "/configuration/security",
+////                        "/webjars/**", ).permitAll()
+////                        .anyRequest().authenticated()); // 일단 임시로 허용
