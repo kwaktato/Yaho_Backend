bf89dbedfdaa40886f904895f78a9f614dbdcbe1
gayeon7877 <gayean01@gmail.com>
2024-08-19 10:54:11 +0900

    ✨ [Feature] 이전 승률 불러오기

--- src/main/java/com/example/yaho/repository/RecordRepository.java
+++ src/main/java/com/example/yaho/repository/RecordRepository.java
@@ -2,6 +2,12 @@ package com.example.yaho.repository;
 
 import com.example.yaho.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    @Query("SELECT r FROM Record r WHERE r.memberId = :memberId ORDER BY r.id DESC")
+    Optional<Record> findLatestRecordByMemberId(@Param("memberId") Long memberId);
 }
